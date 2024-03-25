4ca774e6d794eff96c444d785da6aec6f5ce1f3d
Ghous Ahmed <160832874+ghous-01@users.noreply.github.com>
2024-03-25 08:43:10 +0500

    Number Guessing Game

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answers = await inquirer.prompt([
+  {
+    message: "Please type a number between 1-10:",
+    type: "number",
+    name: "userGuessednumber",
+  },
+]);
+
+if (answers.userGuessednumber === randomNumber) {
+  console.log("Congratulations you guessed the right number.");
+} else console.log("You guessed the wrong number please try again");
